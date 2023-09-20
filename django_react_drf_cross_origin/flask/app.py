d758fdb05c6bd34565e0a00ad393f37beea11a23
hernancontigiani <hernan4790@gmail.com>
2023-09-19 23:50:01 -0300

    Flask test

--- django_react_drf_cross_origin/flask/app.py
+++ django_react_drf_cross_origin/flask/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # Configura CORS para permitir todas las solicitudes
 
 @app.route('/flask/cookies')
 def read_cookies():
