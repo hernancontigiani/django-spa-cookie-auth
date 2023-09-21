e87f406137bbcecb3387704dead2915786af294f
hernancontigiani <hernan4790@gmail.com>
2023-09-20 22:20:20 -0300

    Ampliar tiempo token

--- django_react_drf_jwt/backend/djangocookieauth/settings.py
+++ django_react_drf_jwt/backend/djangocookieauth/settings.py
@@ -87,7 +87,7 @@ REST_FRAMEWORK = {
 
 from datetime import timedelta
 SIMPLE_JWT = {
-     'ACCESS_TOKEN_LIFETIME': timedelta(seconds=120),
+     'ACCESS_TOKEN_LIFETIME': timedelta(seconds=600),
      'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
      'ROTATE_REFRESH_TOKENS': True,
      'BLACKLIST_AFTER_ROTATION': True
